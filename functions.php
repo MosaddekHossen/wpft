1c069ea705e631d032104b000264e2fbe3fca986
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-03-31 03:05:42 +0600

    footer widgets register

--- functions.php
+++ functions.php
@@ -31,9 +31,54 @@ function harry_theme_support()
             'side-menu' => __('Side Menu', 'harry'),
         )
     );
+
+    remove_theme_support('widgets-block-editor');
 }
 add_action("after_setup_theme", "harry_theme_support");
 
+// harry_widgets
+function harry_widgets()
+{
+    register_sidebar(array(
+        'name'          => __('Footer Widget 01', 'harry'),
+        'id'            => 'footer-widget-01',
+        'description'   => __('Widgets in this area will be shown footer.', 'harry'),
+        'before_widget' => '<div id="%1$s" class="footer__widget mb-50 footer-col-1 %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="footer__widget-title">',
+        'after_title'   => '</h3>',
+    ));
+    register_sidebar(array(
+        'name'          => __('Footer Widget 02', 'harry'),
+        'id'            => 'footer-widget-02',
+        'description'   => __('Widgets in this area will be shown footer.', 'harry'),
+        'before_widget' => '<div id="%1$s" class="footer__widget mb-50 footer-col-2 %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="footer__widget-title">',
+        'after_title'   => '</h3>',
+    ));
+    register_sidebar(array(
+        'name'          => __('Footer Widget 03', 'harry'),
+        'id'            => 'footer-widget-03',
+        'description'   => __('Widgets in this area will be shown footer.', 'harry'),
+        'before_widget' => '<div id="%1$s" class="footer__widget mb-50 footer-col-3 %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="footer__widget-title">',
+        'after_title'   => '</h3>',
+    ));
+    register_sidebar(array(
+        'name'          => __('Footer Widget 04', 'harry'),
+        'id'            => 'footer-widget-04',
+        'description'   => __('Widgets in this area will be shown footer.', 'harry'),
+        'before_widget' => '<div id="%1$s" class="footer__widget mb-50 footer-col-4 %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="footer__widget-title">',
+        'after_title'   => '</h3>',
+    ));
+}
+add_action('widgets_init', 'harry_widgets');
+
+// nec files
 include_once("inc/common/scripts.php");
 // Kirki class condition
 if (class_exists('Kirki')) {
