431f43a0377a7bd4e506eb117b3670796da9731b
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-04-15 02:57:45 +0600

    added pagination

--- single.php
+++ single.php
@@ -14,61 +14,66 @@ get_header();
                                 <div class="postbox__main-wrapper">
                                     <?php if (have_posts()) : ?>
                                         <?php while (have_posts()) : the_post(); ?>
-                                            <?php echo get_template_part('/template-parts/content', get_post_format()); ?>
+                                            <?php echo get_template_part('/template-parts/content', get_post_format());
+                                            $prev_post = get_previous_post();
+                                            $next_post = get_next_post();
+                                            ?>
+                                            <?php if (!empty($prev_post) && !empty($next_post)) : ?>
+                                                <div class="postbox__more-navigation white-bg d-none d-md-flex justify-content-between flex-wrap mb-40">
+                                                    <div class="postbox__more-left d-flex align-items-center">
+                                                        <div class="postbox__more-icon">
+                                                            <a href="<?php echo get_the_permalink($prev_post); ?>">
+                                                                <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path d="M7 12.9718L2.06061 8.04401C1.47727 7.46205 1.47727 6.50975 2.06061 5.92778L7 1" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                                                </svg>
+                                                            </a>
+                                                        </div>
+                                                        <div class="postbox__more-content">
+                                                            <p>Previous Article</p>
+                                                            <h4>
+                                                                <a href="<?php echo get_the_permalink($prev_post); ?>"><?php echo get_the_title($prev_post); ?></a>
+                                                            </h4>
+                                                        </div>
+                                                    </div>
+                                                    <div class="postbox__more-right d-flex align-items-center">
+                                                        <div class="postbox__more-content">
+                                                            <p>Next Article</p>
+                                                            <h4>
+                                                                <a href="<?php echo get_the_permalink($next_post); ?>"><?php echo get_the_title($next_post); ?></a>
+                                                            </h4>
+                                                        </div>
+                                                        <div class="postbox__more-icon">
+                                                            <a href="<?php echo get_the_permalink($next_post); ?>">
+                                                                <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path d="M1 12.9718L5.93939 8.04401C6.52273 7.46205 6.52273 6.50975 5.93939 5.92778L1 1" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                                                </svg>
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="postbox__author d-sm-flex align-items-start white-bg mb-95">
+                                                    <div class="postbox__author-thumb">
+                                                        <a href="#">
+                                                            <img src="assets/img/users/user-14.jpg" alt="">
+                                                        </a>
+                                                    </div>
+                                                    <div class="postbox__author-content">
+                                                        <h3 class="postbox__author-title">
+                                                            <a href="#">Colene Landin</a>
+                                                        </h3>
+                                                        <p>Digital content wrangler | UX enthusiast | Recovering educator & GIF hockey fan Oxford comma or death.!</p>
+
+                                                        <div class="postbox__author-social d-flex align-items-center">
+                                                            <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                                                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                                                            <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            <?php endif; ?>
                                         <?php endwhile; ?>
                                     <?php endif; ?>
-                                    <div class="postbox__more-navigation white-bg d-none d-md-flex justify-content-between flex-wrap mb-40">
-                                        <div class="postbox__more-left d-flex align-items-center">
-                                            <div class="postbox__more-icon">
-                                                <a href="blog-details.html">
-                                                    <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M7 12.9718L2.06061 8.04401C1.47727 7.46205 1.47727 6.50975 2.06061 5.92778L7 1" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                                    </svg>
-                                                </a>
-                                            </div>
-                                            <div class="postbox__more-content">
-                                                <p>Previous Article</p>
-                                                <h4>
-                                                    <a href="blog-details.html">Traveling Solo Is Awesome</a>
-                                                </h4>
-                                            </div>
-                                        </div>
-                                        <div class="postbox__more-right d-flex align-items-center">
-                                            <div class="postbox__more-content">
-                                                <p>Next Article</p>
-                                                <h4>
-                                                    <a href="blog-details.html">A Beautiful Sunday Morning</a>
-                                                </h4>
-                                            </div>
-                                            <div class="postbox__more-icon">
-                                                <a href="blog-details.html">
-                                                    <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M1 12.9718L5.93939 8.04401C6.52273 7.46205 6.52273 6.50975 5.93939 5.92778L1 1" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                                                    </svg>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="postbox__author d-sm-flex align-items-start white-bg mb-95">
-                                        <div class="postbox__author-thumb">
-                                            <a href="#">
-                                                <img src="assets/img/users/user-14.jpg" alt="">
-                                            </a>
-                                        </div>
-                                        <div class="postbox__author-content">
-                                            <h3 class="postbox__author-title">
-                                                <a href="#">Colene Landin</a>
-                                            </h3>
-                                            <p>Digital content wrangler | UX enthusiast | Recovering educator & GIF hockey fan Oxford comma or death.!</p>
-
-                                            <div class="postbox__author-social d-flex align-items-center">
-                                                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                                                <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                                                <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
-                                            </div>
-                                        </div>
-                                    </div>
 
                                     <div class="postbox__related mb-65">
                                         <h3 class="postbox__related-title">You may also like</h3>
