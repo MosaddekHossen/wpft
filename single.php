a54a64add0baa5816c3aca86a511e527bd89e3b8
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-04-15 23:39:27 +0600

    Added dynamic comment and views

--- single.php
+++ single.php
@@ -118,15 +118,17 @@ get_header();
                                                                                         <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                                                             <path d="M7.5 14C11.0899 14 14 11.0899 14 7.5C14 3.91015 11.0899 1 7.5 1C3.91015 1 1 3.91015 1 7.5C1 11.0899 3.91015 14 7.5 14Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                                                                                             <path d="M7.5 3.59961V7.49961L10.1 8.79961" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                                                                        </svg> January 21, 2022
+                                                                                        </svg> <?php echo get_the_date(); ?>
                                                                                     </span>
                                                                                 </div>
                                                                             </div>
                                                                             <h3 class="blog__title-10">
-                                                                                <a href="blog-details.html"><?php the_title(); ?></a>
+                                                                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                                                                             </h3>
 
-                                                                            <p>Lorem ipsum is simply dummy text ipsum been dummy...</p>
+                                                                            <p><?php
+                                                                                echo wp_trim_words(get_the_content(), 9, '...');
+                                                                                ?></p>
                                                                         </div>
                                                                         <div class="blog__content-10-bottom d-flex align-items-center justify-content-between">
                                                                             <div class="blog__meta-author-10 d-flex align-items-center">
@@ -136,20 +138,20 @@ get_header();
                                                                                     </a>
                                                                                 </div>
                                                                                 <div class="blog__meta-author-content-10">
-                                                                                    <span>By<a href="#"> Jason</a></span>
+                                                                                    <span>By<a href="<?php echo esc_url(get_author_posts_url($author_id)); ?>"> <?php echo get_the_author(); ?></a></span>
                                                                                 </div>
                                                                             </div>
                                                                             <div class="blog__meta-10 blog-meta-10-2">
                                                                                 <span>
                                                                                     <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                                                         <path d="M12.9718 6.66668C12.9741 7.54659 12.769 8.4146 12.3732 9.20001C11.9039 10.1412 11.1825 10.9328 10.2897 11.4862C9.39697 12.0396 8.36813 12.3329 7.31844 12.3333C6.4406 12.3356 5.57463 12.13 4.79106 11.7333L1 13L2.26369 9.20001C1.86791 8.4146 1.66281 7.54659 1.6651 6.66668C1.66551 5.61452 1.95815 4.58325 2.51025 3.68838C3.06236 2.79352 3.85211 2.0704 4.79106 1.60002C5.57463 1.20331 6.4406 0.997725 7.31844 1.00002H7.65099C9.03729 1.07668 10.3467 1.66319 11.3284 2.64726C12.3102 3.63132 12.8953 4.94378 12.9718 6.33334V6.66668Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                                                                    </svg>35
+                                                                                    </svg><?php get_comments_number(); ?>
                                                                                 </span>
                                                                                 <span>
                                                                                     <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                                                         <path d="M11.0443 7.00397C11.0443 8.43962 9.88694 9.59974 8.45466 9.59974C7.02238 9.59974 5.86499 8.43962 5.86499 7.00397C5.86499 5.56832 7.02238 4.4082 8.45466 4.4082C9.88694 4.4082 11.0443 5.56832 11.0443 7.00397Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                                                                                         <path d="M8.45466 13C11.0082 13 13.3881 11.4918 15.0446 8.88157C15.6956 7.85921 15.6956 6.14078 15.0446 5.11843C13.3881 2.50816 11.0082 1 8.45466 1C5.90115 1 3.52126 2.50816 1.86474 5.11843C1.21371 6.14078 1.21371 7.85921 1.86474 8.88157C3.52126 11.4918 5.90115 13 8.45466 13Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                                                                    </svg>234,5k
+                                                                                    </svg><?php display_post_views(); ?>
                                                                                 </span>
                                                                             </div>
                                                                         </div>
@@ -404,153 +406,6 @@ get_header();
     </div>
 </section>
 <!-- postbox details area end -->
-
-<!-- postbox area start -->
-<section class="postbox__area grey-bg-4 pt-120 pb-120">
-    <div class="container">
-        <div class="row">
-            <div class="col-xxl-8 col-lg-8">
-                <div class="postbox__wrapper">
-                    <?php if (have_posts()) : ?>
-                        <?php while (have_posts()) : the_post(); ?>
-                            <?php echo get_template_part('/template-parts/content', get_post_format()); ?>
-                        <?php endwhile; ?>
-                    <?php endif; ?>
-                    <div class="tp-pagination tp-pagination-style-2 mt-20">
-                        <?php harry_blog_navigation(); ?>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xxl-4 col-lg-4">
-                <div class="sidebar__wrapper pl-40">
-                    <div class="sidebar__widget mb-20">
-                        <div class="sidebar__widget-content">
-                            <div class="sidebar__search">
-                                <form action="#">
-                                    <div class="sidebar__search-input">
-                                        <input type="text" placeholder="Enter your keywords...">
-                                        <button type="submit">
-                                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M9.55 18.1C14.272 18.1 18.1 14.272 18.1 9.55C18.1 4.82797 14.272 1 9.55 1C4.82797 1 1 4.82797 1 9.55C1 14.272 4.82797 18.1 9.55 18.1Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                                <path d="M19.0002 19.0002L17.2002 17.2002" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                            </svg>
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="sidebar__widget mb-45">
-                        <div class="sidebar__widget-content">
-                            <div class="sidebar__author">
-                                <div class="sidebar__author-thumb">
-                                    <img src="assets/img/blog/sidebar/sidebar-author.jpg" alt="">
-                                </div>
-                                <div class="sidebar__author-content">
-                                    <h3 class="sidebar__author-title">Colene Landin</h3>
-                                    <p>Lorem ipsum dolor ametare elit consectetur adipiscing Aenean pellentesque.</p>
-
-                                    <div class="sidebar__author-social d-flex align-items-center justify-content-center">
-                                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                                        <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                                        <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="sidebar__widget mb-45">
-                        <h3 class="sidebar__widget-title">Category</h3>
-                        <div class="sidebar__widget-content">
-                            <ul>
-                                <li><a href="blog.html">Business <span>10</span></a></li>
-                                <li><a href="blog.html">Cleaning <span>08</span></a></li>
-                                <li><a href="blog.html">Consultant <span>24</span></a></li>
-                                <li><a href="blog.html">Creative <span>37</span></a></li>
-                                <li><a href="blog.html">Technology <span>103</span></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="sidebar__widget mb-45">
-                        <h3 class="sidebar__widget-title">Recent Post</h3>
-                        <div class="sidebar__widget-content">
-                            <div class="sidebar__post">
-                                <div class="rc__post d-flex align-items-center">
-                                    <div class="rc__post-thumb">
-                                        <a href="blog-details.html"><img src="assets/img/blog/sidebar/blog-sm-1.jpg" alt=""></a>
-                                    </div>
-                                    <div class="rc__post-content">
-                                        <h3 class="rc__post-title">
-                                            <a href="blog-details.html">Business meeting 2021 in San Francisco</a>
-                                        </h3>
-                                        <div class="rc__meta">
-                                            <span>
-                                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M7.5 14C11.0899 14 14 11.0899 14 7.5C14 3.91015 11.0899 1 7.5 1C3.91015 1 1 3.91015 1 7.5C1 11.0899 3.91015 14 7.5 14Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                                    <path d="M7.5 3.59961V7.49961L10.1 8.79961" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                                </svg>July 21, 2022
-                                            </span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="rc__post d-flex align-items-center">
-                                    <div class="rc__post-thumb">
-                                        <a href="blog-details.html"><img src="assets/img/blog/sidebar/blog-sm-2.jpg" alt=""></a>
-                                    </div>
-                                    <div class="rc__post-content">
-                                        <h3 class="rc__post-title">
-                                            <a href="blog-details.html">Developing privacy user-centric apps</a>
-                                        </h3>
-                                        <div class="rc__meta">
-                                            <span>
-                                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M7.5 14C11.0899 14 14 11.0899 14 7.5C14 3.91015 11.0899 1 7.5 1C3.91015 1 1 3.91015 1 7.5C1 11.0899 3.91015 14 7.5 14Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                                    <path d="M7.5 3.59961V7.49961L10.1 8.79961" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                                </svg>July 21, 2022
-                                            </span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="rc__post d-flex align-items-center">
-                                    <div class="rc__post-thumb">
-                                        <a href="blog-details.html"><img src="assets/img/blog/sidebar/blog-sm-3.jpg" alt=""></a>
-                                    </div>
-                                    <div class="rc__post-content">
-                                        <h3 class="rc__post-title">
-                                            <a href="blog-details.html">Starting and Growing Web Design in 2022</a>
-                                        </h3>
-                                        <div class="rc__meta">
-                                            <span>
-                                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M7.5 14C11.0899 14 14 11.0899 14 7.5C14 3.91015 11.0899 1 7.5 1C3.91015 1 1 3.91015 1 7.5C1 11.0899 3.91015 14 7.5 14Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                                    <path d="M7.5 3.59961V7.49961L10.1 8.79961" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                                                </svg>July 21, 2022
-                                            </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="sidebar__widget mb-40">
-                        <h3 class="sidebar__widget-title">Tags</h3>
-                        <div class="sidebar__widget-content">
-                            <div class="tagcloud">
-                                <a href="#">Techology</a>
-                                <a href="#">Food</a>
-                                <a href="#">Personality</a>
-                                <a href="#">Life Style</a>
-                                <a href="#">Travel</a>
-                                <a href="#">Nature</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<!-- postbox area end -->
 
 <?php
 get_footer();
