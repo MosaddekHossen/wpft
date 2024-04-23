61ec01a8d5d9a4a6f8725584fa4fd37452848b61
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-04-23 22:15:15 +0600

    Added dynamic sidebar

--- single.php
+++ single.php
@@ -1,5 +1,6 @@
 <?php
 get_header();
+$post_col = is_active_sidebar('blog-sidebar') ? 'col-xxl-8 col-lg-8' : 'col-xxl-12 col-lg-12';
 ?>
 
 <!-- postbox details area start -->
@@ -10,7 +11,7 @@ get_header();
                 <div class="postbox__wrappers">
                     <div class="postbox__mains">
                         <div class="row">
-                            <div class="col-lg-8">
+                            <div class="<?php echo esc_attr($post_col); ?>">
                                 <div class="postbox__main-wrapper">
                                     <?php if (have_posts()) : ?>
                                         <?php while (have_posts()) : the_post(); ?>
@@ -54,6 +55,8 @@ get_header();
                                                 <?php echo get_template_part('/template-parts/biography'); ?>
                                             <?php endif; ?>
                                         <?php endwhile; ?>
+                                    <?php else : ?>
+                                        <?php echo get_template_part('/template-parts/content-none'); ?>
                                     <?php endif; ?>
 
                                     <div class="postbox__related mb-65">
