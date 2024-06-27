e75ba99283d65e8e9f36cf00d589604fe5e9ba11
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-06-27 16:27:21 +0600

    Product shop page updated

--- inc/woo.php
+++ inc/woo.php
@@ -4,6 +4,8 @@ remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
 remove_action('woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30);
 remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
 remove_action('woocommerce_shop_loop_header', 'woocommerce_product_taxonomy_archive_header', 10);
+// 
+remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar', 10);
 
 remove_action('woocommerce_shop_loop_header', 'woocommerce_template_loop_product_link_open', 10);
 remove_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10);
