bb7b8ed1c78701d1ede646f27bbf2faca110021e
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-07-06 11:38:03 +0600

    Added list view

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -135,14 +135,44 @@ if (woocommerce_product_loop()) {
 								 *
 								 * @hooked woocommerce_pagination - 10
 								 */
-								do_action('woocommerce_after_shop_loop'); ?>
+								do_action('woocommerce_after_shop_loop');
+								?>
 							</div>
 						</div>
 						<div class="tab-pane fade" id="nav-list" role="tabpanel" aria-labelledby="nav-list-tab">
 							<!-- shop list -->
 							<div class="product__list-wrapper mb-30">
 								<div class="row">
+									<?php
+									/**
+									 * Hook: woocommerce_before_shop_loop.
+									 *
+									 * @hooked woocommerce_output_all_notices - 10
+									 * @hooked woocommerce_result_count - 20
+									 * @hooked woocommerce_catalog_ordering - 30
+									 */
+									do_action('woocommerce_before_shop_loop');
 
+									if (wc_get_loop_prop('total')) {
+										while (have_posts()) {
+											the_post();
+
+											/**
+											 * Hook: woocommerce_shop_loop.
+											 */
+											do_action('woocommerce_shop_loop');
+
+											wc_get_template_part('content', 'product-list');
+										}
+									}
+
+									/**
+									 * Hook: woocommerce_after_shop_loop.
+									 *
+									 * @hooked woocommerce_pagination - 10
+									 */
+									do_action('woocommerce_after_shop_loop');
+									?>
 								</div>
 							</div>
 						</div>
