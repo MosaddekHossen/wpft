8875e44826e516fc7d4317ca5033712f55de06c0
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-10-21 16:01:39 +0600

    update

--- functions.php
+++ functions.php
@@ -38,6 +38,7 @@ function harry_theme_support()
             'main-menu' => __('Main Menu', 'harry'),
             'side-menu' => __('Side Menu', 'harry'),
             'footer-menu' => __('Footer Menu', 'harry'),
+            'language-menu' => __('Language Menu', 'harry'),
         )
     );
 
